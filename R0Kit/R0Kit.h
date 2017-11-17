7b0e321668d844b4890b9aad882c8017f3d9231a
Robert M. Lefkowitz <r0ml@mac.com>
2017-11-17 17:57:11 -0500

    splitting r0kit

---
+++ R0Kit/R0Kit.h
@@ -0,0 +1,19 @@
+//
+//  R0Kit.h
+//  R0Kit
+//
+//  Created by Robert M. Lefkowitz on 11/17/17.
+//  Copyright © 2017 Semasiology. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for R0Kit.
+FOUNDATION_EXPORT double R0KitVersionNumber;
+
+//! Project version string for R0Kit.
+FOUNDATION_EXPORT const unsigned char R0KitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <R0Kit/PublicHeader.h>
+
+
