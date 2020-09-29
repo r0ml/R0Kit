5db4a660e610e4442029a061a6b37eff00e9f483
Robert M. Lefkowitz <r0ml@mac.com>
2020-09-29 16:35:48 -0400

    additional NSImage extensions

--- Sources/R0Kit/NSImage.swift
+++ Sources/R0Kit/NSImage.swift
@@ -7,7 +7,8 @@ import AppKit
 import CoreGraphics
 
 extension NSImage {
-  func stroke(normalizedPath : CGPath, color : CGColor, lineWidth: CGFloat) {
+/*
+ func stroke(normalizedPath : CGPath, color : CGColor, lineWidth: CGFloat) {
     self.lockFocus()
     var p3 = CGAffineTransform(scaleX: self.size.width, y: self.size.height)
     let p2 = normalizedPath.copy(using: &p3)!
@@ -18,12 +19,29 @@ extension NSImage {
     cx.strokePath()
     self.unlockFocus()
   }
-
+*/
+  
   // Create an NSImage from a CIImage
-  convenience init(im : CIImage) {
+  convenience init(ciImage im : CIImage) {
     let rep = NSCIImageRep(ciImage: im)
     self.init(size: rep.size)
     self.addRepresentation(rep)
+  }
+
+  public var pngData : Data? { get {
+    guard let cgImage = self.cgImage(forProposedRect: nil, context: nil, hints: nil) else { return nil }
+    let newRep = NSBitmapImageRep(cgImage: cgImage)
+    newRep.size = self.size
+    guard let pngd = newRep.representation(using: .png, properties: [:]) else { return nil }
+    return pngd
+  }
+  }
+
+  public var ciImage : CIImage?  {
+    if let imageData = tiffRepresentation {
+      return CIImage(data: imageData)
+    }
+    return nil
   }
 
 }
